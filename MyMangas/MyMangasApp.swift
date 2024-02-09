f643cb04eba65c728e28a921eb023d55eb918a80
Blas Santomé Ocampo <bsantome@mercadona.es>
2024-02-09 15:59:00 +0100

    Initial Commit

---
+++ MyMangas/MyMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMangasApp.swift
+//  MyMangas
+//
+//  Created by Blas Santome Ocampo on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
